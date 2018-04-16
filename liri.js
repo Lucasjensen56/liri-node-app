e6e780cb57030a3c5b9a8b855a65510ac84121ee
Lucas Jensen <lucasjensen56@gmail.com>
2018-04-16 12:56:18 -0500

    Update liri.js
    Added some twitter note API update

--- liri.js
+++ liri.js
@@ -1,1 +1,16 @@
 require("dotenv").config();
+
+
+// twitter
+
+client.get(path, params, callback);
+
+client.get('favorites/list', function(error, tweets, response) {
+  if(error) throw error;
+  console.log(tweets);  // The favorites. 
+  console.log(response);  // Raw response object. 
+});
+
+// 
+GET https://api.twitter.com/1.1/statuses/lookup.json?id=20,432656548536401920
+
