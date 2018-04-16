19db031db3a199d722696bcf7c6b7d55eb2ac46d
Lucas Jensen <lucasjensen56@gmail.com>
2018-04-16 18:35:02 -0500

    created switch statement to display tweets with argument

--- liri.js
+++ liri.js
@@ -13,6 +13,7 @@ var client = new twitter(keys.twitter);
 var liriCommanLine = (process.argv[2]);
 
 
+
 function displayTweets () {
 
   var screenName = {screen_name: 'jensen_lucas'};
@@ -38,19 +39,18 @@ function displayTweets () {
    
 };
 
-displayTweets()
 
 
+// if (liriCommanLine === "my-tweets") {
+//   displayTweets();
+
+// }; 
 
 
+switch(liriCommanLine) {
+  case"my-tweets":
+    displayTweets();
+  break;
+};
 
-// twitter
-
-// client.get(path, params, callback);
-
-
-
-
-// // 
-// GET https://api.twitter.com/1.1/statuses/lookup.json?id=20,432656548536401920
 
