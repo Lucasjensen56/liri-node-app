e87f060bbf07397a8418893694fc26a26d1af37e
Lucas Jensen <lucasjensen56@gmail.com>
2018-04-16 17:00:52 -0500

    added dotenv npm, linked key.js, created variable for liri command

--- liri.js
+++ liri.js
@@ -2,20 +2,32 @@ require("dotenv").config();
 
 var Twitter = require('twitter');
 
+var fs = require('fs');
+
+
+var keys = require('./keys.js');
+
+var liriCommanLine = (process.argv[2]);
+
+
+console.log(process.argv)
+
+
 
 
 
 // twitter
 
-client.get(path, params, callback);
-
-client.get('favorites/list', function(error, tweets, response) {
-  if(error) throw error;
-  console.log(tweets);  // The favorites. 
-  console.log(response);  // Raw response object. 
-});
+// client.get(path, params, callback);
 
 
-// 
-GET https://api.twitter.com/1.1/statuses/lookup.json?id=20,432656548536401920
+// client.get('favorites/list', function(error, tweets, response) {
+//   if(error) throw error;
+//   console.log(tweets);  // The favorites. 
+//   console.log(response);  // Raw response object. 
+// });
+
+
+// // 
+// GET https://api.twitter.com/1.1/statuses/lookup.json?id=20,432656548536401920
 
